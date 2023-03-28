3a5c7f321da5756d882a7205feb86e9e92c8d77f
Alimhidayat <hidayatalim27@gmail.com>
2023-03-28 11:40:49 +0700

    v1.2.1

---
+++ proses.php
@@ -0,0 +1,24 @@
+<?php
+  $angka1 = $_POST['angka1'];
+  $angka2 = $_POST['angka2'];
+  $operator = $_POST['operator'];
+
+  switch($operator) {
+    case '+':
+      $hasil = $angka1 + $angka2;
+      break;
+    case '-':
+      $hasil = $angka1 - $angka2;
+      break;
+    case '*':
+      $hasil = $angka1 * $angka2;
+      break;
+    case '/':
+      $hasil = $angka1 / $angka2;
+      break;
+    default:
+      $hasil = "Operasi tidak valid";
+  }
+
+  echo "Hasil operasi $angka1 $operator $angka2 adalah $hasil";
+?>
